0726b8b208d4324ab77e006a9611c7e69bbd30d3
apyd <arturpydych@gmail.com>
2024-02-06 00:05:14 +0100

    feat: implement os operations

--- operations.js
+++ operations.js
@@ -1,13 +1,13 @@
 import { normalize, join } from 'node:path'
-import { EOL, homedir } from 'node:os'
-import { chdir, exit, stdout, cwd } from 'node:process'
+import { EOL, homedir, userInfo, arch, cpus, availableParallelism } from 'node:os'
+import { chdir, exit, stdout, cwd,  } from 'node:process'
 import { readdir, stat } from 'node:fs/promises'
 import { getUsername } from './utils/index.js'
-import { COMMAND } from './constants/index.js'
+import { COMMANDS, ERROR_MESSAGES, FILE_TYPES, OS_PARAM } from './constants/index.js'
 
 export const getOperation = operationName => {
   return {
-    [COMMAND.CD]: async (dedicatedFolder) => {
+    [COMMANDS.cd]: async (dedicatedFolder) => {
       try {
         const currentPath = cwd()
         const updatedPath = join(currentPath, dedicatedFolder)
@@ -20,40 +20,40 @@ export const getOperation = operationName => {
           throw new Error()
         }
       } catch (error) {
-        stdout.write(`Operation failed: ${error}${EOL}`)
+        stdout.write(`${ERROR_MESSAGES.OPERATION_FAILED}: ${error}${EOL}`)
       }
     },
-    [COMMAND.LS]: async() => {
+    [COMMANDS.ls]: async() => {
       try {
         const currentPath = cwd()
         const dirContent = await readdir(currentPath, { withFileTypes: true })
         const mappedDirContent = dirContent.map(item => {
           return {
             name: item.name,
-            type: item.isDirectory() ? 'directory' : 'file'
+            type: item.isDirectory() ? FILE_TYPES.DIRECTORY : FILE_TYPES.FILE
           }
         })
         const sortedAlphabeticallyContent = mappedDirContent.sort((a, b) => a.name.localeCompare(b.name))
         const sortedByTypeContent = sortedAlphabeticallyContent.sort((a, b) => {
-          if(a.type === 'directory' && b.type === 'file') {
+          if(a.type === FILE_TYPES.DIRECTORY && b.type === FILE_TYPES.FILE) {
             return -1
           }
-          if(a.type === 'file' && b.type === 'directory') {
+          if(a.type === FILE_TYPES.FILE && b.type === FILE_TYPES.DIRECTORY) {
             return 1
           }
           return 0
         })
         console.table(sortedByTypeContent)
       } catch (error) {
-        stdout.write(`Operation failed: ${error}${EOL}`)
+        stdout.write(`${ERROR_MESSAGES.OPERATION_FAILED}: ${error}${EOL}`)
       }
     },
-    [COMMAND.EXIT]: () => {
+    [COMMANDS['.exit']]: () => {
       const username = getUsername()
       stdout.write(`${EOL}Thank you for using File Manager, ${username}!${EOL}`)
       exit(0)
     },
-    [COMMAND.UP]: () => {
+    [COMMANDS.up]: () => {
       const currentPath = cwd()
       const homeDir = homedir()
 
@@ -63,6 +63,37 @@ export const getOperation = operationName => {
       } else {
         process.stdout.write(`You are already in the home directory${EOL}`)
       }
-    }
-  }[operationName] || process.stdout.write(`Invalid input${EOL}`)
+    },
+    [COMMANDS.os]: (param) => {
+      let dataToPrint = ''
+      switch (param) {
+        case OS_PARAM.EOL:
+          dataToPrint = `End of line: ${EOL}`
+          break
+        case OS_PARAM.CPUS:
+          let summary = ''
+          const cpusList = cpus()
+          summary += `Amount of CPUs: ${availableParallelism}${EOL}`
+          cpusList.forEach((cpu, index) => {
+            summary += `Model ${index}: ${cpu.model}${EOL}`
+            summary += `Clock rate: ${cpu.speed / 1000} GHZ${EOL}${EOL}`
+          })
+          dataToPrint = summary
+          break
+        case OS_PARAM.HOMEDIR:
+          dataToPrint = homedir()
+          break
+        case OS_PARAM.USERNAME:
+          dataToPrint = userInfo().username
+          break
+        case OS_PARAM.ARCHITECTURE:
+            dataToPrint = arch()
+            break
+        default:
+          dataToPrint = ERROR_MESSAGES.OPERATION_FAILED
+      }
+      console.log(dataToPrint)
+    },
+
+  }[operationName] || process.stdout.write(`${ERROR_MESSAGES.INVALID_INPUT}${EOL}`)
 }
