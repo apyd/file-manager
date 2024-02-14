b95c660f0d00a74697fc1373efc980dce49bb4f4
apyd <arturpydych@gmail.com>
2024-02-14 14:59:47 +0100

    fix: handle error on compress/decompress read/write stream

--- operations/compress.js
+++ operations/compress.js
@@ -1,55 +1,49 @@
-import { createReadStream, createWriteStream } from 'node:fs';
+import { createReadStream, createWriteStream, unlink } from 'node:fs';
 import { createBrotliCompress, createBrotliDecompress } from 'node:zlib';
 import { ERROR_MESSAGES } from '../constants/index.js';
 import { getCorrectPath } from '../utils/index.js';
 
-export const compress = (pathToInputFile, pathToOutputFile) => {
+const handleStreamError = (readStream, writeStream, outputPath, errorMessage) => {
+  console.error(errorMessage);
+  readStream.destroy();
+  writeStream.destroy();
+  unlink(outputPath, () => {});
+};
+
+export const compress = async(pathToInputFile, pathToOutputFile) => {
   const normalizedInputPath = getCorrectPath(pathToInputFile);
   const normalizedOutputPath = getCorrectPath(pathToOutputFile);
 
-  try {
     const readStream = createReadStream(normalizedInputPath, { flags: 'r' });
     const writeStream = createWriteStream(normalizedOutputPath, { flags: 'wx' });
     const brotliStream = createBrotliCompress();
 
     readStream.on('error', (error) => {
-      console.error(`${ERROR_MESSAGES.OPERATION_FAILED}. Error reading input file: ${error}`);
-    });
-  
+      handleStreamError(readStream, writeStream, normalizedOutputPath, `${ERROR_MESSAGES.OPERATION_FAILED}. Error reading input file: ${error}`);
+    })
+
     writeStream.on('error', (error) => {
-      console.error(`${ERROR_MESSAGES.OPERATION_FAILED}. Error writing output file: ${error}`);
-    });
-  
-    readStream.pipe(brotliStream).pipe(writeStream).on('finish', () => {
-      console.log(`File compressed successfully under: ${normalizedOutputPath}`);
-    });
-  }
-  catch (error) {
-    console.log(`${ERROR_MESSAGES.OPERATION_FAILED}: ${error}`)
-  }
+      handleStreamError(readStream, writeStream, normalizedOutputPath, `${ERROR_MESSAGES.OPERATION_FAILED}. Error reading output file: ${error}`);
+    })
+
+    readStream.pipe(brotliStream).pipe(writeStream);
+
 }
 export const decompress = (pathToInputFile, pathToOutputFile) => {
   const normalizedInputPath = getCorrectPath(pathToInputFile);
   const normalizedOutputPath = getCorrectPath(pathToOutputFile);
 
-  try {
-    const readStream = createReadStream(normalizedInputPath, { flags: 'r' });
-    const writeStream = createWriteStream(normalizedOutputPath, { flags: 'wx' });
-    const brotliStream = createBrotliDecompress();
+  const readStream = createReadStream(normalizedInputPath, { flags: 'r' });
+  const writeStream = createWriteStream(normalizedOutputPath, { flags: 'wx' });
+  const brotliStream = createBrotliDecompress();
 
-    readStream.on('error', (error) => {
-      console.error(`${ERROR_MESSAGES.OPERATION_FAILED}. Error reading input file: ${error}`);
-    });
+  readStream.on('error', (error) => {
+    handleStreamError(readStream, writeStream, normalizedOutputPath, `${ERROR_MESSAGES.OPERATION_FAILED}. Error reading input file: ${error}`);
+  });
+
+  writeStream.on('error', (error) => {
+    handleStreamError(readStream, writeStream, normalizedOutputPath, `${ERROR_MESSAGES.OPERATION_FAILED}. Error reading outputs file: ${error}`);
+  });
   
-    writeStream.on('error', (error) => {
-      console.error(`${ERROR_MESSAGES.OPERATION_FAILED}. Error writing output file: ${error}`);
-    });
-  
-    readStream.pipe(brotliStream).pipe(writeStream).on('finish', () => {
-      console.log(`File compressed successfully under: ${normalizedOutputPath}`);
-    });
-  }
-  catch (error) {
-    console.log(`${ERROR_MESSAGES.OPERATION_FAILED}: ${error}`)
-  }
+  readStream.pipe(brotliStream).pipe(writeStream);
 }
